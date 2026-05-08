7d12f1586f78d2ccdda9c8a85900764b0f2430e3
Paulo Alberto <141840002+PauloAlbertodev@users.noreply.github.com>
2026-05-08 15:40:35 -0300

    atualizacao pastas

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -1,16 +1,22 @@
 ﻿namespace Ex02;
 
 class Program
+        //Escreva um programa que leia três notas (decimais) e mostre a média, formatando a saída.
 {
     static void Main(string[] args)
-    {
-         double nota1 = 9;
-         double nota2 = 6;
-         double nota3 = 2;
+    {    
+       Console.WriteLine("Digite a primeira nota;");
+       double nota1 = double.Parse(Console.ReadLine());
 
-         double soma = (nota1+nota2+nota3);
-         double media =(soma / 3);
-         Console.WriteLine($"A media é:{media:f2}");
+       Console.WriteLine("Digite a segunda nota;");
+       double nota2 = double.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite a terceira nota;");
+       double nota3 = double.Parse(Console.ReadLine());
+
+       double media = (nota1+nota2+nota3)/3;
+
+       Console.WriteLine($"Média: {media:F1}.");
 
 
 
--- Ex03/Program.cs
+++ Ex03/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace Ex03;
+                    /*Escreva um programa que leia uma palavra e mostre quantos caracteres ela possui,
+                    ignorando espaços no início e fim (texto.Length).*/
 
 class Program
-{
+{     
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("digite uma palavra");
+        string texto = Console.ReadLine();
+        texto = texto.Trim();
+        texto.Length;
+
     }
 }
